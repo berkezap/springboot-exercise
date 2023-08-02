0e1ae68ca632c74433e85c7cdc4f53989d3bba13
berkezap <ihsanberke.ozsap@std.yeditepe.edu.tr>
2023-08-02 13:43:03 +0300

    rekt

---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <img >
+        public/logo1.png
+      </img>
+        <p>
+          Mustafa
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Gardaşım
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
